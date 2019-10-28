3d68b170ede92821425204135c09864f429d7bfa
aajour <ahmed.ajourr@gmail.com>
2019-10-28 15:47:36 +0200

    UI reay

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,1 +1,23 @@
-console.log("Hi");
+var buttons = document.getElementsByClassName("myButton1");
+var currentPlayer = document.getElementById("currentPlayer");
+var board = [[], [], []];
+
+function changeCurrentPlayer() {
+  if (currentPlayer.textContent === "X") {
+    currentPlayer.textContent = "O";
+  } else {
+    currentPlayer.textContent = "X";
+  }
+}
+
+function handleBtnClick(id) {
+  var btn = buttons[id];
+  test(btn);
+  changeCurrentPlayer();
+}
+
+function test(btn) {
+  btn.value = currentPlayer.textContent;
+  btn.disabled = true;
+  btn.setAttribute("style", "cursor:not-allowed;color:red");
+}
